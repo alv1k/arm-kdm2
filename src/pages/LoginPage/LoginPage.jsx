8d2eb8dcd5d40eb6cc471c74747d4cad0b8a5db3
al_v1k <alekseevaalena442@gmail.com>
2025-04-22 17:17:23 +0900

    logout page fixes

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -40,12 +40,12 @@ const LoginPage = () => {
   }
 
   return (
-    <main className="min-h-fit"> 
-      <div className="lg:flex min-h-fit relative">
-        <div className="lg:w-1/2 w-full absolute h-screen bg-[#F6F8FF]">
-          <img className="lg:blur-none blur-xs mt-62" src="/src/assets/images/bg.png" alt="" />
+    <main className="min-h-fit h-screen"> 
+      <div className="lg:flex min-h-fit h-screen lg:static relative">
+        <div className="lg:w-1/2 lg:static w-full absolute h-screen bg-[#F6F8FF]">
+          <img className="lg:blur-none lg:mt-0 blur-xs mt-62" src="/src/assets/images/bg.png" alt="" />
         </div>
-        <div className="lg:w-1/2 w-full absolute">
+        <div className="lg:w-1/2 lg:static w-full absolute">
           <div className="bg-white p-8 xl:mt-50 my-20 mx-auto lg:w-2/3 w-[90vw] rounded-2xl text-center">
             <img className="mx-auto" src="/src/assets/images/logo.png" alt="logo" />
             <p className="mt-4 text-[#4B5E9D] lg:text-2xl md:text-2xl text-sm uppercase font-bold lg:ms-5 md:ms-5 ms-2 my-auto" style={{fontFamily: 'PT Sans'}}>комдрагметалл рс(я)</p>
